eeb0b0eafcdcff7d8328410226276baf143344b1
matveyt <matthewtarasov@yandex.ru>
2025-08-10 20:09:37 +0300

    Bug fix and code refactor

--- hex2c.c
+++ hex2c.c
@@ -1,11 +1,8 @@
 //
 // hex2c
 //
-// Convert between Intel HEX, Binary and C Include
-// Intel HEX: only data record type (00) supported, max size is 64KB
-//
-// $ make CFLAGS=-O LDFLAGS=-s hex2c
-// $ hex2c file.ihx >file.h
+// Convert between Intel HEX, Binary and C Include format
+// Intel HEX: only data record type (00) supported (64KB max)
 //
 // https://github.com/matveyt/hex2c
 //
@@ -23,21 +20,12 @@
 #include <io.h>
 #endif // _WIN32
 
-#define PROGRAM_NAME "hex2c"
-
-// Intel HEX file
+// HEX format
 #define MAX_ADDRESS UINT16_MAX
-#define MIN_LINE    (1 + 2 * 5) // count(1) + address(2) + type(1) + checksum(1)
+#define MIN_LINE    11  // colon(1) count(2) address(4) type(2) checksum(2)
 #define MAX_LINE    (MIN_LINE + 2 * UINT8_MAX)
-// C Include file
-#define C_HEADER    "const uint8_t " PROGRAM_NAME "[%zu] = "
-// min and max
-#ifndef min
-#define min(a, b)   (((a) < (b)) ? (a) : (b))
-#endif // min
-#ifndef max
-#define max(a, b)   (((a) > (b)) ? (a) : (b))
-#endif // max
+
+static const char program_name[] = "hex2c";
 
 // user options
 unsigned o_padding = 0;
@@ -47,45 +35,54 @@ unsigned o_wrap = 0;
 /*noreturn*/
 void usage(void)
 {
-    fputs("\
-Usage: " PROGRAM_NAME " [OPTION]... FILE\n\
-Convert between Intel HEX, Binary and C Include format.\n\
-\n\
--B, --from-binary   FILE has no specific format\n\
--H, --from-hex      FILE has Intel HEX format [default]\n\
--b, --binary        Binary dump output\n\
--c, --c             C Include output [default]\n\
--h, --hex           Intel HEX format output\n\
--o, --output=FILE   set output file name\n\
--p, --padding=NUM   extra space on line\n\
--s, --silent        suppress messages\n\
--w, --wrap=NUM      maximum output bytes per line\n\
-\n\
-If no --output is given then writes to stdout.\n\
-Intel HEX format is 8-bit only (64KB max).\n\
-", stdout);
-    exit(EXIT_SUCCESS);
+    fprintf(stdout,
+"Usage: %s [OPTION]... FILE\n"
+"Convert between Intel HEX, Binary and C Include format.\n"
+"\n"
+"-B, --from-binary   FILE has no specific format\n"
+"-H, --from-hex      FILE has Intel HEX format [default]\n"
+"-b, --binary        Binary dump output\n"
+"-c, --c             C Include output [default]\n"
+"-h, --hex           Intel HEX format output\n"
+"-o, --output=FILE   set output file name\n"
+"-p, --padding=NUM   extra space on line\n"
+"-s, --silent        suppress messages\n"
+"-w, --wrap=NUM      maximum output bytes per line\n"
+"\n"
+"If no --output is given then writes to stdout.\n"
+"Intel HEX format is 8-bit only (64KB max).\n",
+        program_name);
+    exit(EXIT_FAILURE);
 }
 
-void warn(unsigned lineno, const char* msg)
+void warn(unsigned lineno, const char msg[])
 {
     if (!o_silent)
         fprintf(stderr, "Warning (line %u): %s\n", lineno, msg);
 }
 
 /*noreturn*/
-void die(const char* msg)
+void die(const char msg[])
 {
     if (!o_silent)
-        fprintf(stderr, "Exiting due to error: '%s'\n", msg);
+        fprintf(stderr, "Error exit: %s\n", msg);
     exit(EXIT_FAILURE);
 }
 
-FILE* xfopen(const char* fname, const char* mode)
+static inline size_t xmin(size_t x, size_t y)
+{
+    return (x < y) ? x : y;
+}
+static inline size_t xmax(size_t x, size_t y)
+{
+    return (x > y) ? x : y;
+}
+
+FILE* xfopen(const char fname[], const char mode[])
 {
     FILE* f = fopen(fname, mode);
     if (f == NULL)
-        die("opening file");
+        die("open file");
     return f;
 }
 
@@ -97,36 +94,25 @@ void* xmalloc(size_t sz)
     return p;
 }
 
-static inline FILE* as_binary(FILE* f)
-{
-#if defined(_WIN32)
-    _setmode(fileno(f), _O_BINARY);
-#endif // _WIN32
-    return f;
-}
-
-// convert hex number from string
-static uint_fast32_t hex_scan(char line[], size_t length)
+// read hex number from string
+unsigned hex_scan(char line[], unsigned length)
 {
     char t = line[length];
     line[length] = '\0';
-    uint_fast32_t number = strtoul(line, NULL, 16);
+    unsigned number = (unsigned)strtoul(line, NULL, 16);
     line[length] = t;
     return number;
 }
-static inline uint_fast8_t hex_scan8(char line[])
-{
-    return hex_scan(line, 2);
-}
-static inline uint_fast16_t hex_scan16(char line[])
-{
-    return hex_scan(line, 4);
-}
+static inline unsigned hex_scan8(char line[])
+{ return hex_scan(line, 2); }
+static inline unsigned hex_scan16(char line[])
+{ return hex_scan(line, 4); }
 
 // parsed data chunk
 typedef struct {
     uint8_t data[UINT8_MAX];
-    size_t address, length;
+    unsigned length;
+    size_t address;
 } CHUNK;
 
 // parse one Intel HEX line
@@ -134,11 +120,11 @@ typedef struct {
 int hex_parse(CHUNK* pc, char line[])
 {
     // init chunk
-    pc->address = 0;
     pc->length = 0;
+    pc->address = 0;
 
     // empty line not allowed
-    size_t length = strlen(line);
+    unsigned length = strlen(line);
     if (length == 0 || line[0] != ':')
         return -1;
 
@@ -149,25 +135,24 @@ int hex_parse(CHUNK* pc, char line[])
         --length;
 
     // check number of hex digits
-    if (length < MIN_LINE || (length & 1) == 0)
+    if (length < MIN_LINE || length > MAX_LINE || !(length & 1))
         return -1;
-    for (size_t i = 1; i < length; ++i)
+    for (unsigned i = 1; i < length; ++i)
         if (!isxdigit(line[i]))
             return -1;
 
     // get count, address and type
-    uint_fast8_t count = hex_scan8(&line[1]);
-    uint_fast16_t address = hex_scan16(&line[3]);
-    uint_fast8_t type = hex_scan8(&line[7]);
-    if ((size_t)MIN_LINE + 2 * count != length
-        || (unsigned)address + count > MAX_ADDRESS + 1)
+    unsigned count = hex_scan8(&line[1]);
+    unsigned address = hex_scan16(&line[3]);
+    unsigned type = hex_scan8(&line[7]);
+    if (MIN_LINE + 2 * count != length || address + count > MAX_ADDRESS + 1)
         return -1;
 
     // get data and checksum
-    uint_fast8_t sum = count + (uint_fast8_t)(address >> 8) + (uint_fast8_t)address
-        + type + hex_scan8(&line[length - 2]);
-    for (size_t cb = 0, i = 9; i < length - 2; i += 2) {
-        uint_fast8_t t = hex_scan8(&line[i]);
+    unsigned sum = count + (address >> 8) + address + type
+        + hex_scan8(&line[length - 2]);
+    for (unsigned cb = 0, i = 9; i < length - 2; i += 2) {
+        unsigned t = hex_scan8(&line[i]);
         pc->data[cb++] = t;
         sum += t;
     }
@@ -186,8 +171,8 @@ size_t load_hex(uint8_t** bin, FILE* f)
 {
     *bin = memset(xmalloc(MAX_ADDRESS + 1), UINT8_MAX, MAX_ADDRESS + 1);
     size_t sz = 0;
-    bool found_eof = false;
 
+    bool found_eof = false;
     for (unsigned lineno = 1; !found_eof; ++lineno) {
         char line[MAX_LINE + 3];    // CR+LF+NUL
         if (fgets(line, sizeof(line), f) == NULL) {
@@ -200,7 +185,7 @@ size_t load_hex(uint8_t** bin, FILE* f)
         case 0:
             // hex_parse() ensures that we never get past MAX_ADDRESS
             memcpy(*bin + chunk.address, chunk.data, chunk.length);
-            sz = max(sz, chunk.address + chunk.length);
+            sz = xmax(sz, chunk.address + chunk.length);
         break;
         case 1:
             found_eof = true;
@@ -221,23 +206,23 @@ size_t load_hex(uint8_t** bin, FILE* f)
     if (sz > 0)
         *bin = realloc(*bin, sz);
     else
-        free(bin);
+        free(*bin);
     return sz;
 }
 
 // format output as Intel HEX file
-static void dump_hex(uint8_t data[], size_t sz, FILE* f)
+void dump_hex(uint8_t data[], size_t sz, FILE* f)
 {
     // 64KB max
-    sz = min(sz, MAX_ADDRESS + 1);
+    sz = xmin(sz, MAX_ADDRESS + 1);
     // user options
     unsigned wrap = (o_wrap != 0) ? o_wrap : 16;
 
     for (size_t i = 0; i < sz; i += wrap) {
-        unsigned cb = min(wrap, sz - i);
+        unsigned cb = xmin(wrap, sz - i);
         // : count address type(00)
         fprintf(f, ":%02X%04zX00", cb, i);
-        uint_fast8_t sum = cb + (i >> 8) + i;
+        unsigned sum = cb + (i >> 8) + i;
         // data
         for (unsigned j = 0; j < cb; ++j) {
             fprintf(f, "%02X", data[i + j]);
@@ -252,21 +237,24 @@ static void dump_hex(uint8_t data[], size_t sz, FILE* f)
 }
 
 // format output as C Include
-static void dump_c(uint8_t data[], size_t sz, FILE* f)
+void dump_c(uint8_t data[], size_t sz, FILE* f)
 {
     // user options
     unsigned wrap = (o_wrap != 0) ? o_wrap : 8;
     unsigned padding = (o_padding != 0) ? o_padding : 4;
 
     // header
-    fprintf(f, C_HEADER "{\n", sz);
+    fprintf(f,
+"// made with %s\n"
+"const uint8_t %s_data[%zu] = {\n",
+        program_name, program_name, sz);
 
     for (size_t i = 0; i < sz; i += wrap) {
         // leading space
         fprintf(f, "%*c", padding, ' ');
 
         // data
-        unsigned cb = min(wrap, sz - i);
+        unsigned cb = xmin(wrap, sz - i);
         for (unsigned j = 0; j < cb; ++j)
             fprintf(f, "0x%02x, ", data[i + j]);
 
@@ -340,6 +328,7 @@ int main(int argc, char* argv[])
     FILE* fin = xfopen(argv[optind], "r");
     FILE* fout = (output == NULL || strcmp(output, "-") == 0) ?
         stdout : xfopen(output, "w");
+    free(output);
 
     // binary image
     uint8_t* bin = NULL;
@@ -348,7 +337,9 @@ int main(int argc, char* argv[])
     // read in
     switch (fmt_in) {
     case 'B':
-        as_binary(fin);
+#if defined(_WIN32)
+        _setmode(fileno(fin), _O_BINARY);
+#endif // _WIN32
         fseek(fin, 0, SEEK_END);
         sz = ftell(fin);
         bin = xmalloc(sz);
@@ -364,7 +355,10 @@ int main(int argc, char* argv[])
     if (sz > 0) {
         switch (fmt_out) {
         case 'b':
-            fwrite(bin, 1, sz, as_binary(fout));
+#if defined(_WIN32)
+            _setmode(fileno(fout), _O_BINARY);
+#endif // _WIN32
+            fwrite(bin, 1, sz, fout);
         break;
         case 'c':
             dump_c(bin, sz, fout);
@@ -373,10 +367,9 @@ int main(int argc, char* argv[])
             dump_hex(bin, sz, fout);
         break;
         }
+        free(bin);
     }
 
-    free(bin);
-    free(output);
     fclose(fout);
     fclose(fin);
     return 0;
